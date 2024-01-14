15f99156d7c774127974828f2c2295f543430ea6
alexanderpierce <takaaki.110706@gmail.com>
2024-01-14 19:29:55 +0900

    Install serverless-better-credentials and Remove unused settings of serverless.ts

--- serverless.ts
+++ serverless.ts
@@ -3,28 +3,10 @@ import type { AWS } from '@serverless/typescript';
 const serverlessConfiguration: AWS = {
   service: 'appsync-serverless-ts',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild', 'serverless-appsync-plugin'],
+  plugins: ['serverless-better-credentials'],
   provider: {
     name: 'aws',
     region: 'ap-northeast-1',
-    runtime: 'nodejs14.x',
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-    },
-  },
-  package: { individually: true },
-  custom: {
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
-      concurrency: 10,
-    },
   },
 };
 
