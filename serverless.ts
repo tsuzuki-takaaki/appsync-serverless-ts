54bac8693653068176d2a3cf12d89a9ee8739286
alexanderpierce <takaaki.110706@gmail.com>
2024-01-14 20:14:16 +0900

    Add appsync vtl sample

--- serverless.ts
+++ serverless.ts
@@ -1,12 +1,41 @@
-import type { AWS } from '@serverless/typescript';
+// import type { AWS } from '@serverless/typescript';
 
-const serverlessConfiguration: AWS = {
+const serverlessConfiguration: any = { // TODO: Type -> const serverlessConfiguration: AWS にappSyncを入れれるようにする https://github.com/sid88in/serverless-appsync-plugin/blob/dbaf899e0fc2c1543cbd980074f67f043b3b604f/src/types/serverless.d.ts ?
   service: 'appsync-serverless-ts',
   frameworkVersion: '3',
-  plugins: ['serverless-better-credentials'],
+  plugins: ['serverless-better-credentials', 'serverless-appsync-plugin'],
   provider: {
     name: 'aws',
     region: 'ap-northeast-1',
+  },
+  appSync: {
+    name: 'hogeMaru',
+    schema: 'schema.graphql',
+    authentication: {
+      type: 'API_KEY'
+    },
+    dataSources: {
+      hogeMaruSource: { // this is for mappingTemplates' dataSource
+        type: 'AMAZON_DYNAMODB',
+        config: {
+          tableName: 'HogeMaru'
+        }
+      }
+    },
+    resolvers: {
+      'Query.listHogeMarus': {
+        kind: 'UNIT',
+        dataSource: 'hogeMaruSource',
+        request: './mapping-templates/list_hoge_marus/request.vtl',
+        response: './mapping-templates/list_hoge_marus/response.vtl'
+      },
+      'Mutation.putHogeMaru': {
+        kind: 'UNIT',
+        dataSource: 'hogeMaruSource',
+        request: './mapping-templates/put_hoge_maru/request.vtl',
+        response: './mapping-templates/put_hoge_maru/response.vtl'
+      }
+    }
   },
   resources: { // TODO
     Resources: {
