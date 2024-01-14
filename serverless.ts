7c3178741964f80da94cf67cf461ce8f008aa797
alexanderpierce <takaaki.110706@gmail.com>
2024-01-14 19:30:44 +0900

    Add simple dynamoDB setting

--- serverless.ts
+++ serverless.ts
@@ -8,6 +8,19 @@ const serverlessConfiguration: AWS = {
     name: 'aws',
     region: 'ap-northeast-1',
   },
+  resources: { // TODO
+    Resources: {
+      Table: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: 'HogeMaru',
+          AttributeDefinitions: [ { AttributeName: 'id', AttributeType: 'S' } ],
+          KeySchema: [ { AttributeName: 'id', KeyType: 'HASH' } ],
+          ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
+        },
+      }
+    }
+  }
 };
 
 module.exports = serverlessConfiguration;
