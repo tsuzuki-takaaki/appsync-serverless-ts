c069489cc06e2a5947980c8941713bd08309fac7
alexanderpierce <takaaki.110706@gmail.com>
2024-01-14 17:24:39 +0900

    Remove unused files and settings

--- serverless.ts
+++ serverless.ts
@@ -1,25 +1,18 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
-
 const serverlessConfiguration: AWS = {
   service: 'appsync-serverless-ts',
   frameworkVersion: '3',
   plugins: ['serverless-esbuild'],
   provider: {
     name: 'aws',
+    region: 'ap-northeast-1',
     runtime: 'nodejs14.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
-    },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
   },
-  // import the function via paths
-  functions: { hello },
   package: { individually: true },
   custom: {
     esbuild: {
