5fda0a3fd443408c5ad80dedea4665366fa05db9
alexanderpierce <takaaki.110706@gmail.com>
2024-01-16 15:50:37 +0900

    Add simple Lambda setting

--- serverless.ts
+++ serverless.ts
@@ -1,13 +1,26 @@
 // import type { AWS } from '@serverless/typescript';
 
-const serverlessConfiguration: any = { // TODO: Type -> const serverlessConfiguration: AWS にappSyncを入れれるようにする https://github.com/sid88in/serverless-appsync-plugin/blob/dbaf899e0fc2c1543cbd980074f67f043b3b604f/src/types/serverless.d.ts ?
+import hello from '@functions/hello';
+
+const serverlessConfiguration = { // TODO: Type -> const serverlessConfiguration: AWS にappSyncを入れれるようにする https://github.com/sid88in/serverless-appsync-plugin/blob/dbaf899e0fc2c1543cbd980074f67f043b3b604f/src/types/serverless.d.ts ?
   service: 'appsync-serverless-ts',
   frameworkVersion: '3',
-  plugins: ['serverless-better-credentials', 'serverless-appsync-plugin'],
+  plugins: ['serverless-better-credentials', 'serverless-appsync-plugin', 'serverless-esbuild'],
   provider: {
     name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
     region: 'ap-northeast-1',
   },
+  functions: { hello },
+  package: { individually: true },
   appSync: {
     name: 'hogeMaru',
     schema: 'schema.graphql',
@@ -41,6 +54,18 @@ const serverlessConfiguration: any = { // TODO: Type -> const serverlessConfigur
         response: './mapping-templates/put_hoge_maru/response.vtl'
       }
     }
+  },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
   },
   resources: { // TODO
     Resources: {
